38e04ef3e95992c00184c7935bcb273422e9d530
vered shani <shanive21@gmail.com>
2011-12-18 14:41:48 +0200

    added missing files

---
+++ src/com/ATP/AgentState.java
@@ -0,0 +1,64 @@
+package com.ATP;
+/**
+ * representation of agent's state on ATPstate
+ * include agent's current position and current vehicle id
+ * @author Vered
+ *
+ */
+public class AgentState {
+	private int position;
+	private int vehicle;
+    private double pathCost;
+
+	public AgentState(int initial){
+		this.position = initial; //initial state
+		this.vehicle = -1; //has no vehicle at the beginning
+        this.pathCost  = 0.0;
+	}
+
+	public AgentState(AgentState other){
+		this.position = other.getAgentPosition();
+		this.vehicle = other.getAgentVehicleId();
+	}
+
+	public int getAgentPosition(){
+		return this.position;
+	}
+
+	public int getAgentVehicleId(){
+		return this.vehicle;
+	}
+
+	public void setAgentPosition(int pos){
+		this.position = pos;
+	}
+
+	public void setAgentVehicleId(int id){
+		this.vehicle = id;
+	}
+
+	@Override
+	public int hashCode(){
+		int hash = 7;
+		hash = 31 * hash + this.position;
+		hash = 31 * hash + this.vehicle;
+		return hash;
+	}
+
+	@Override
+	public boolean equals(Object other){
+		if (other == null)
+			return false;
+		AgentState state = (AgentState)other;
+		return ((this.position == state.position) &&
+				(this.vehicle == state.vehicle));
+	}
+
+	@Override
+	public Object clone(){
+		AgentState other = new AgentState(this.position);
+		other.vehicle = this.vehicle;
+		return other;
+	}
+
+}
--- src/com/game/GameHeuristic.java
+++ src/com/game/GameHeuristic.java
@@ -2,6 +2,9 @@ package com.game;
 
 import com.tree.GreedyHeuristic;
 
-public class GameHeuristic extends GreedyHeuristic{
+public class GameHeuristic extends GreedyHeuristic {
+  public GameHeuristic(int agentID, int goal) {
+    super(agentID, goal);
+  }
 
 }
