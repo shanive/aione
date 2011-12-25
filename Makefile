f06d749099652a8ce2979f325926f65acceedfa1
David Tolpin <david.tolpin@gmail.com>
2011-12-25 11:38:28 +0200

    distance heuristic

--- Makefile
+++ Makefile
@@ -4,8 +4,9 @@ SOURCES=\
   src/iai/atp/World.java src/iai/atp/InOut.java \
   src/iai/atp/State.java src/iai/atp/Move.java \
   src/iai/atp/Agent.java src/iai/atp/Tour.java \
+  src/iai/atp/Heur.java \
   src/iai/atp/HumanAgent.java \
-  src/iai/atp/Game.java
+  src/iai/atp/Game.java \
 
 all:
 	javac -cp bin -d bin -sourcepath src  $(SOURCES)
---
+++ src/iai/atp/Heur.java
@@ -0,0 +1,37 @@
+package iai.atp;
+
+import iai.lib.Graph;
+import iai.lib.WEdge;
+import iai.lib.Dijkstra;
+
+/** Cost heuristic, Dijkstra-based */
+class Heur {
+	/** 1/maximum-speed */
+	private final double speedfactor;
+	/** Dijkstra distance table */
+	private final Dijkstra dijkstra;
+
+	/** initializes the heuristic for an agent
+	 * @param world the wolrd
+	 * @param it the agent index
+	 */
+	Heur(World world, int it) {
+		/* first, find the maximum clear speed */
+		double maxcspeed = 0.0;
+		for(int iv=0; iv!=world.vehicles.length; ++iv)
+			if(world.vehicles[iv].cspeed > maxcspeed)
+				maxcspeed = world.vehicles[iv].cspeed;
+		speedfactor = 1.0/maxcspeed; /* implicit assert maxcspeed > 0 */
+
+		/* then, compute the distances from the target node */
+		dijkstra = new Dijkstra(world.roads, world.travellers[it].t);
+	}
+
+	/** returns a heuristic (optimistic) cost for a node
+	 * @param v the current node
+	 * @return distance from v to the target
+	 */
+	double cost(int v) {
+		return speedfactor*dijkstra.dist(v);
+	}
+}
