b554cd22b078c1a80ed7200047076b8af6c738fa
David Tolpin <david.tolpin@gmail.com>
2011-12-27 01:17:13 +0200

    better pdfs, with vehicles this time

--- tools/graph2dot.py
+++ tools/graph2dot.py
@@ -16,13 +16,13 @@ for line in sys.stdin:
 		continue
 	if words[0]=="#V": 
 		vcl_at,speed,factor = words[1:]
-		print "  node [shape=box];"
-		print "  Vcl%s -- V%s [label=\"%s/%s\"];" % (ivcl, vcl_at, float(speed), float(speed)*float(factor))
+		print "  node [shape=box,color=grey];"
+		print "  Vcl%s -- V%s [label=\"%s/%s\",color=grey];" % (ivcl, vcl_at, float(speed), float(speed)*float(factor))
 		ivcl=ivcl+1
 	elif words[0]=="#E":
 		v_from,v_to,weight,state = words[1:]
 		weight = weight[1:]
-		print "  node [shape=ellipse];"
+		print "  node [shape=ellipse,color=black];"
 		print "  V%s -- V%s [label=%s, style=%s];" % (v_from, v_to, weight, (state=="F" and "dashed" or "bold"))
 
 # vehicles
